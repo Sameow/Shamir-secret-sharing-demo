87a54307323c0bc1b41d38f201f9de51000d7e4c
121884D <121884D@mymail.nyp.edu.sg>
2014-10-08 15:05:32 +0800

    Revert "Changed argument order (modulus before secret)"
    This reverts commit ae1acdbfc5bf88c6b3068a50d0e8b5c6cc269123.

--- src/engine/SecretShare.java
+++ src/engine/SecretShare.java
@@ -433,7 +433,7 @@ public class SecretShare
     public CombineOutput combine(final List<ShareInfo> usetheseshares)
     {
         CombineOutput ret = null;
-  
+
         sanityCheckPublicInfos(publicInfo, usetheseshares);
 
         if (publicInfo.getK() > usetheseshares.size())
